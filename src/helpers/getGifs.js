a1e193cea6da818e2d84a7eff05a9970005a067d
Roberto Quintero <langdonhp435@gmail.com>
2021-07-16 15:22:43 -0500

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+export const getGifs = async (category) => {
+  const apiKey = "bClJgFBjO0rd7Za0G0ILFBPDf7bfssgK";
+  const limit = 10;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=${limit}&api_key=${apiKey}`;
+
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+  return gifs;
+};
